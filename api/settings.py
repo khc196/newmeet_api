a231a1a791a98c96a135fffa4c571051c9b2a3d7
khc196 <khc146@gmail.com>
2019-05-21 18:38:30 +0900

    whitenoise update

--- api/settings.py
+++ api/settings.py
@@ -175,7 +175,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [
     os.path.join(PROJECT_ROOT,'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
