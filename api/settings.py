0cfd0cbedda8683d1fcd7d64124760ba54d9bc4b
khc196 <khc146@gmail.com>
2019-05-22 15:50:42 +0900

    update settings.py

--- api/settings.py
+++ api/settings.py
@@ -188,7 +188,7 @@ AWS_STORAGE_BUCKET_NAME = 'newmeet'
 AWS_REGION = 'ap-northeast-2'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAM
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Static Setting
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
