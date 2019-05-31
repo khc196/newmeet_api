2a42841a99a8a13fbfab2c816da5f19b7d992ef2
khc196 <khc146@gmail.com>
2019-05-31 13:32:19 +0900

    update

--- api/settings.py
+++ api/settings.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
 ]
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
 )
 SITE_ID=1
 ACCOUNT_AUTHENTICATION_METHOD = 'id'
