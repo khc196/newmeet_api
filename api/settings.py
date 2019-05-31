97d6a3625d7aaf459b317bf5aa911c720c6985b0
khc196 <khc146@gmail.com>
2019-05-31 13:46:18 +0900

    update

--- api/settings.py
+++ api/settings.py
@@ -70,11 +70,10 @@ MIDDLEWARE = [
 ]
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 SITE_ID=1
 ACCOUNT_AUTHENTICATION_METHOD = 'id'
-ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-ACCOUNT_USERNAME_REQUIRED = False
 SOCIALACCOUNT_ADAPTER = 'authorization.adapter.SocialAccountAdapter'
 
 AUTH_USER_MODEL = 'authorization.User'
